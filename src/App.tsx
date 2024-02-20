13d0f43c233aa75b32ab8377863795e93843a820
Amal C P <acp070146@gmail.com>
2024-02-20 21:26:11 +0530

    page not found updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
-import NotFound from "./pages/notFound";
+
 import Home from "./pages/home";
 import SignUp from "./pages/authentication/signUp";
 import SignIn from "./pages/authentication/signIn";
 import ProfileCreate from "./pages/authentication/profileCreate";
 import Profile from "./pages/authentication/profile";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
 	const router = createBrowserRouter([
@@ -38,30 +39,7 @@ function App() {
 			path: "/profile",
 			element: <Profile/>,
 		},
-		// {
-		// 	path: "/",
-		// 	element: <PrivateRoute />,
-		// 	children: [
-		// 		{
-		// 			path: "/profile",
-		// 			element: <Profile />,
-		// 		},
-		// 		{
-		// 			path: "/",
-		// 			element: <RoleChecker allowedRoles={[Roles.ADMIN]} />,
-		// 			children: [
-		// 				{
-		// 					path: "/payment-status",
-		// 					element: <Admin />,
-		// 				},
-		// 				{
-		// 					path: "/payment-status/:id",
-		// 					element: <PaymentStatus />,
-		// 				},
-		// 			],
-		// 		},
-		// 	],
-		// },
+		
 	]);
 	return (
 		<div className="App">
