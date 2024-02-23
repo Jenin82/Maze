62647d1427ed76237bde1b751539fc91b4d03798
Amal C P <acp070146@gmail.com>
2024-02-23 17:19:23 +0530

    hidden google

--- src/services/PrivateRoutes.tsx
+++ src/services/PrivateRoutes.tsx
@@ -20,7 +20,7 @@ export const PrivateRoute = () => {
 		const session = await supabase.auth.getSession();
 		if (!session.data.session?.access_token) {
 			await supabase.auth.signOut();
-			toast.error("Please sign in first");
+			// toast.error("Please sign in first");
 			navigate("/signin");
 		} else if (!roles || roles.length === 0) {
 			toast.error("Please update your profile");
